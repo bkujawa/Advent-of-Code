9698170a1bea892fff2da8e4daf0e7c34db605be
Bartosz Kujawa <bakj@dgs.com>
2019-12-12 23:01:22 +0100

    Day 8.

--- AdventOfCode/AdventOfCode/InputStrings.cs
+++ AdventOfCode/AdventOfCode/InputStrings.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode
+﻿namespace AdventOfCode
 {
     static class InputStrings
     {
         public static readonly string Day1 = "InputDayOne.txt";
         public static readonly string Day2 = "InputDayTwo.txt";
         public static readonly string Day4 = "InputDayFour.txt";
+        public static readonly string Day8 = "InputDayEight.txt";
     }
 }
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/*
+﻿/*
     Santa has become stranded at the edge of the Solar System while delivering presents to other planets!
     To accurately calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs you to bring him measurements from fifty stars.
     Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. 
     Each puzzle grants one star.
     Good luck!
  */
+
 namespace AdventOfCode
 {
     class Program
@@ -21,6 +17,7 @@ namespace AdventOfCode
             puzzleSolver = new Day1(InputStrings.Day1);
             puzzleSolver = new Day2(InputStrings.Day2);
             puzzleSolver = new Day4(InputStrings.Day4);
+            puzzleSolver = new Day8(InputStrings.Day8);
         }
     }
 }
